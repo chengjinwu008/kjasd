aae223026ee9ba965e766c03aaa650828bba3c1b
GitHub Action <action@github.com>
2022-04-22 03:53:28 +0000

    Template cleanup

--- src/main/kotlin/com/github/chengjinwu008/kjasd/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/chengjinwu008/kjasd/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.chengjinwu008.kjasd.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.chengjinwu008.kjasd.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/chengjinwu008/kjasd/services/MyProjectService.kt
+++ src/main/kotlin/com/github/chengjinwu008/kjasd/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.chengjinwu008.kjasd.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.chengjinwu008.kjasd.MyBundle
 
 class MyProjectService(project: Project) {
 
